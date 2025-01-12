9f72b0430f113fdd7f84c94f41268a64c3460bb8
EwerssonSlv <ewerssonsllv@gmail.com>
2025-01-12 11:21:21 -0300

    Endpoints

--- src/main/kotlin/com/ewersson/dashboard_bi_api/config/WebConfig.kt
+++ src/main/kotlin/com/ewersson/dashboard_bi_api/config/WebConfig.kt
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
+            .allowedOrigins("http://127.0.0.1:5500", "https://bi-app-qvw1.onrender.com")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
     }
 }
